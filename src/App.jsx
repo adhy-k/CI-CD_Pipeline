7bd7bedd05160e8d1950405b1973b3ca3397f623
adhy-k <adhykrishna123@gmail.com>
2026-01-08 15:23:18 +0530

    pushng

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,7 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-<<<<<<< HEAD
-     <h1>This is from jenkins pipeline v8</h1>
-=======
-     <h1>This is from jenkins pipeline v3</h1>
->>>>>>> 94efc755a0cdcbdd9229cfe03062b76a30bc4914
+     <h1>This is from jenkins pipeline v1</h1>
     </div>
   )
 }
